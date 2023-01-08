cec6b79e1bc3a2c595352df6bdd60f3a58c9857a
Yang Chao <iyeatse@gmail.com>
2023-01-08 23:01:21 +0800

    Add stub target to avoid manually adding dependencies in Xcode

--- Package.swift
+++ Package.swift
@@ -14,11 +14,23 @@ let package = Package(
     products: [
         .library(
             name: "OpenCV",
-            targets: ["opencv2"]),
+            targets: ["opencv2", "opencv2-dependencies"]),
     ],
     targets: [
         .binaryTarget(name: "opencv2",
                       url: "https://github.com/Yeatse/OpenCV-SPM/releases/download/\(version)/opencv2.xcframework.zip",
                       checksum: checksum),
+        .target(
+            name: "opencv2-dependencies",
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo", .when(platforms: [.iOS])),
+                .linkedFramework("Accelerate", .when(platforms: [.iOS, .macOS])),
+                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedLibrary("c++")
+            ]
+        )
     ]
 )
