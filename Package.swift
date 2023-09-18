26c0672b02564313d85140d5bead36d75158f21c
Kevin Aguilar <47673410+kevin58332@users.noreply.github.com>
2023-09-18 12:17:03 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let version = "4.8.0"
 let checksum = "83d60791fb49b22a9c587d6aded426d213afcfc733a5439b050e5dddd2706292"
 
 let package = Package(
-    name: "opencv2",
+    name: "opencv-spm",
     platforms: [
         .macOS(.v10_13), .iOS(.v11), .macCatalyst(.v13)
     ],
